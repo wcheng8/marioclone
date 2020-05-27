e6508ed193b39beb38b3d975201d3219003d0ed6
Wilkins Cheng <wilkins.cheng@gmail.com>
2020-05-27 14:58:41 +1000

    initial platform game

---
+++ public/js/main.js
@@ -0,0 +1,18 @@
+function loadImage(url) {
+  return new Promise((resolve) => {
+    const image = new Image();
+    image.addEventListener("load", () => {
+      resolve(image);
+    });
+    image.src = url;
+  });
+}
+
+const canvas = document.getElementById("screen");
+const context = canvas.getContext("2d");
+
+context.fillRect(0, 0, 50, 50);
+
+loadImage("/images/tiles.png").then((image) => {
+  context.drawImage(image, 0, 0, 16, 16, 0, 0, 16, 16);
+});
